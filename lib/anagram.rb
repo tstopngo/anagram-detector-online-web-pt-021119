51233d89bfc7e74245dc5cd8e3f9155031a43caa
Tracy Ngo <tngo1206@gmail.com>
2019-03-16 21:33:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,18 +5,14 @@ class Anagram
 attr_accessor :word
   
   def initialize(word)
-    
     @word = word
-  
   end
   
   def match(array)
-    
     array.collect do |anagram|
       if anagram.split.sort == @word.split.sort
         anagram
       end 
-        
     end
   
   end
